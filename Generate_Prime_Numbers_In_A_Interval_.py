a3a8fc182c996b67a60b2045c7326b990e044aa7
21MH1A05B6 <97426967+21MH1A05B6@users.noreply.github.com>
2022-07-03 08:42:11 +0530

    def prime(n):     for i in range(2,(n//2)+1):         if n%i==0:             return False     else:         return True
    n=int(input())
    m=int(input())
    for i in range(n+1,m+1):
        if prime(i):
            print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,12 @@
+def prime(n):
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+
+n=int(input())
+m=int(input())
+for i in range(n+1,m+1):
+    if prime(i):
+        print(i)
