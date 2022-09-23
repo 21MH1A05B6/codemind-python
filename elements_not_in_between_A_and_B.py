3142a5c43f64d2845c5537bb04991c482e12ff96
21MH1A05B6 <97426967+21MH1A05B6@users.noreply.github.com>
2022-09-23 18:40:54 +0530

    n=int(input()) a=list(map(int,input().split())) x,y=map(int,input().split()) s=[] b=0 for i in range(0,n):     if a[i]<x or a[i]>y:         s.append(a[i])         b+=1 if b==0:     print("-1") else:     print(*s)

--- elements_not_in_between_A_and_B.py
+++ elements_not_in_between_A_and_B.py
@@ -1,10 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
-b,c=map(int,input().split())
-x = 0
-for i in a:
-    if i not in range(b,c+1):
-        print(i,end=' ')
-        x = 1
-if x==0:
+x,y=map(int,input().split())
+s=[]
+b=0
+for i in range(0,n):
+    if a[i]<x or a[i]>y:
+        s.append(a[i])
+        b+=1
+if b==0:
     print("-1")
+else:
+    print(*s)
