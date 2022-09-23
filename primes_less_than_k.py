b4b4a47b6461d8932c0cd88161a8aa04c8de0803
21MH1A05B6 <97426967+21MH1A05B6@users.noreply.github.com>
2022-09-23 16:02:47 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1 n=int(input()) a=list(map(int,input().split())) m=int(input()) c=0 for i in a:     if prime(i) and i<=m:         c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,15 @@
+def prime(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    return 1
+n=int(input())
+a=list(map(int,input().split()))
+m=int(input())
+c=0
+for i in a:
+    if prime(i) and i<=m:
+        c+=1
+print(c)
